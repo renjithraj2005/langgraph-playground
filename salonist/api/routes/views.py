53b65380d5d584893b2f40b05ebd5173e4e430ea
Renjith Raj <renjithraj2005@gmail.com>
2025-04-10 22:16:04 +0530

    fixed UI

--- salonist/api/routes/views.py
+++ salonist/api/routes/views.py
@@ -4,4 +4,5 @@ main_bp = Blueprint('main', __name__, template_folder='templates')
 
 @main_bp.route('/')
 def index():
-    return render_template('index.html', name="Blueprint Example")
+    return render_template(
+        'index.html', name="Blueprint Example")
